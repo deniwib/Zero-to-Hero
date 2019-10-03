6e89091cca725ca12b17c5e5d14507b6f2825edb
deniwib <56105174+deniwib@users.noreply.github.com>
2019-10-03 14:14:31 +0800

    Basic arithmetic calculations
    hactoberfest

--- Turbo C++/Sum_of_two_numbers.cpp
+++ Turbo C++/Sum_of_two_numbers.cpp
@@ -1,13 +1,19 @@
 #include <iostream.h>
 #include <conio.h>
 
-void main()
+int main()
 {
-  int a, b, c;
+  int a, b, c, d, e, f;
   cout << "Enter two integers to add\n";
   cin >> a >> b;
-  c = a + b;
-
-  cout <<"Sum of the numbers: " << c << endl;
+  c = a / b;
+  d = a * b;
+  e = a + b;
+  f = a - b;
+  
+  cout <<"division of the numbers: " << c << endl;
+  cout <<"sum of the numbers: " << e << endl;
+  cout <<"subtraction of the numbers: " << f << endl;
+  cout <<"multiplication of the numbers: " << d << endl;
   return 0;
 }
